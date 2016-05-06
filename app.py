305d04162a217929bde47a9846c1388eec231c67
yutong pang <yutongpang@yutongs-iMac.home>
2016-05-06 19:21:46 -0400

    add nvd3

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.config['DEBUG'] = False
 
 @app.route('/')
 def main():
@@ -9,6 +11,14 @@ def main():
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+api = Api(app)
+class Vizq1(Resource):
+  def get(self):
+    return "hello world"
+
+api.add_resource(Vizq1,'/vizq1')
+
 
 if __name__ == '__main__':
   app.run(port=33507)
